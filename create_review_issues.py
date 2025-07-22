4186f28fbbfffee46c601c0e6b7b22e2e46c4ea0
Michael-Baudeur <91018292+Michael-Baudeur@users.noreply.github.com>
2025-07-22 13:43:53 +0200

    Update create_review_issues.py
    Script python création des tâches de review

--- create_review_issues.py
+++ create_review_issues.py
@@ -16,7 +16,33 @@ for item in data["data"]["node"]["items"]["nodes"]:
   status = None
   #parcours des custom fields
   for field in item["fieldValues"]["nodes"]:
+    #check du custom field status
     if field.get("field", {}).get("name") == "Status":
       options = field["field"]["options"]
       option_id = field.get("OptionId")
+      #Match de l'option avec l'option Id
+        for opt in options:
+          if opt["id"] == option_id:
+            status = opt["name"]
+            break
+    if status == "Review":
+      issue_title = f"Review {title}"
+      print(f"Creating issue: {issue_title}")
+      r = requests.post(
+        f"https://api.github.com/repos/{OWNER}/{REPO}/issues",
+        headers={
+          "Authorization": f"Bearer {TOKEN}",
+          "Accept": "application/vnd.github+json"
+        },
+        json={
+          "title": issue_title,
+          "body": f"Issue reveiw auto-created for '{title}'"
+        }
+      }
+      if r.status_code == 201:
+        print(f"✅ Created: {r.json()['html_url']}")
+      else:
+        print(f"❌ Error: {r.status_code} {r.text}")
+        
     
+      
